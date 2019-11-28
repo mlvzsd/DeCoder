8519e890399be2739e4265485c64a8cb3ba4a336
thvzsd <tinhosv@protonmail.com>
2019-11-27 22:54:10 -0400

    move Bloix script

--- scripts/Bloix.gd
+++ scripts/Bloix.gd
@@ -66,7 +66,27 @@ func up():
 	
 	position.y = y if y > 0 else 0 #Don't exit the screen
 	
-
+#Map block code to each name
+func map_code(program):
+	var out_block = true
+	var key_buffer = ""
+	var value_buffer = ""
+	var map
+	
+	for c in program.length():
+		if out_block and c == ":":
+			out_block = false
+		
+		elif c == "}":
+			out_block = true
+			value_buffer += c
+		if out_block:
+			key_buffer += c
+		
+		else:
+			value_buffer += c
+		
+	pass
 # Return the blocks of code of given string or return 0
 func first_parse(program):
 	
