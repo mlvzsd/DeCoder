3bc4a31bb5b8faf5e77c57c44f9d15cffc170198
MarkusRedluxan <41394372+MarkusRedluxan@users.noreply.github.com>
2019-12-14 09:16:54 -0400

    Add files via upload

--- scripts/Bloix.gd
+++ scripts/Bloix.gd
@@ -2,11 +2,7 @@ extends Node2D
 
 const Parser = preload("Parser.gd")
 
-
-
-const WIDTH=280
-const HEIGHT=160
-
+const SIZE=16
 
 enum DIR {
 	DOWN
@@ -15,62 +11,66 @@ enum DIR {
 	RIGHT
 }
 
-# main:{ ahead; ahead; for 7 {ahead; ahead}; turn_left; back; keijo } keijo: { ahead }
-
-# var code = "main:[turn_left ahead/ ahead/ for 3 [ turn_right ] / ahead/ ahead/ back] kaiju:[hentai/ tentaculo/ tentaculo]"
-var code = "main:[ gentoo ] gentoo:[ ahead / ahead ]"
-
-var cur_dir = DIR.DOWN 
-
+var default_position
+var code = "main:{ giradinha } giradinha:{ vira, vira, vira, } vira:{ ahead, ahead, turn_left}"
+var cur_dir : int = DIR.DOWN 
 
 # Called when the node enters the scene tree for the first time
 func _ready():
-	exec(code)
+	default_position = transform.get_origin()
+
+func reset():
+	cur_dir = DIR.DOWN
+	position = default_position
+	
+func start(pos):
+	position = pos
+	default_position = pos
+
 #Execute the given program as string
 func exec(program: String, fun := "main"):
-	var cod = Parser.parse(program, ["[", "]"], "/")
+	var cod = Parser.parse(program, ["{", "}"], ",")
 
 	for cmd in cod[fun]:
+		yield(get_tree().create_timer(.5), "timeout")
+		
 		if cod.keys().has(cmd.cmd):
 			exec(program, cmd.cmd)
 		
 		else:
 			call(cmd.cmd, cmd.args)
-		
-		yield(get_tree().create_timer(1), "timeout")
 
-	pass
 #Go to right
 func right():
 	var x = position.x
 	
-	x += 16
+	x += SIZE
 	
-	position.x = x if x < WIDTH else WIDTH #Don't exit the screen
+	position.x = x
 
 # Go to 16pixel left
 func left():
 	var x = position.x
 	
-	x -= 16
+	x -= SIZE
 	
-	position.x = x if x > 0 else 0 #Don't exit the screen
+	position.x = x
 
 #Go to 16pixel down
 func down():
 	var y = position.y
 	
-	y += 16
+	y += SIZE
 	
-	position.y = y if y < HEIGHT else HEIGHT #Don't exit the screen
+	position.y = y
 
 # Go to 16pixel up
 func up():
 	var y = position.y
 	
-	y -= 16
+	y -= SIZE
 	
-	position.y = y if y > 0 else 0 #Don't exit the screen
+	position.y = y
 
 #func for(code):
 #	pass
@@ -78,11 +78,15 @@ func up():
 # turn direction 90 degree to right
 func turn_right(a):
 	cur_dir = (cur_dir + 1 + 4) % 4
+	#$Sprite.rotation_degrees -= 90
 	print(cur_dir)
+
 # turn direction 90 degree to left
 func turn_left(a):
 	cur_dir = (cur_dir - 1 + 4) % 4
+	#$Sprite.rotation_degrees += 90
 	print(cur_dir)
+	
 # go in oposite of current direction
 func back(a):
 	go((cur_dir - 2 + 4) % 4)
@@ -107,5 +111,3 @@ func go(dir):
 			print("right")
 		var err:
 			print(err, ": unspected value")
-	
-	
--- scripts/LoopCmd.gd
+++ scripts/LoopCmd.gd
@@ -3,41 +3,26 @@ extends Node2D
 const MIN = 0
 const MAX = 999
 
-var moving
-var by_left
 var value = MIN
 
 const NUMS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-func _ready():
-	moving = true
-	by_left = true
-	$LeftDragButton.passby_press = true
-
-# warning-ignore:unused_argument
-func _process(delta):
-	if moving:
-		var new_pos = get_global_mouse_position()
-		new_pos.x += 32 if by_left else -32
-		
-		position = new_pos
-
 func str_android(num: int):
 	var res := ""
-	
+
 	while(num > 0):
 		res = NUMS[num%10] + res
 		num /= 10
-	
+
 	return res if res else "0"
 
 func clamp_android(num: int):
 	if num < MIN:
 		return MIN
-	
+
 	elif num > MAX:
 		return MAX
-	
+
 	return  num
 
 func _on_IncrementButton_pressed():
@@ -48,17 +33,5 @@ func _on_DecrementButton_pressed():
 	value = clamp_android(value - 1)
 	$ValueLabel.text = str_android(value)
 
-func _on_LeftDragButton_pressed():
-	moving = true
-	by_left = true
-	z_index = 1
-	$LeftDragButton.passby_press = false
-
-func _on_RightDragButton_pressed():
-	moving = true
-	by_left = false
-	z_index = 1
-
-func _on_DragButton_released():
-	moving = false
-	z_index = 0
+func _on_FocusButton_pressed():
+	pass
